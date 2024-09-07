8515a615eadfb59922145fae2a883ed4dfc038d4
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-07 21:06:26 +0530

    TrafficSignal.js

---
+++ JavaScript/ConditionalStatements/TrafficSignal.js
@@ -0,0 +1,13 @@
+let color="green";
+if(color == "red")
+{
+    console.log("Stop");
+}
+if(color == "yellow")
+{
+    console.log("Get Ready");
+}
+if(color == "green")
+{
+    console.log("Go");
+}
