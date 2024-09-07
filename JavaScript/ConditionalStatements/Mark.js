364483aa1bdd21eb5b51147de28195f103016eb6
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-07 21:05:48 +0530

    Mark.js

---
+++ JavaScript/ConditionalStatements/Mark.js
@@ -0,0 +1,14 @@
+let mark=110;
+
+if(mark<=50)
+{
+    console.log("You need to improve");
+}
+else if (mark>50 && mark<=70)
+{
+    console.log("Good job");
+}
+else if(mark>70 && mark<=100)
+{
+    console.log("Excellent Performace");
+}
