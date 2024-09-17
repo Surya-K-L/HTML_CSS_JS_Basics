3b61cb2f74186e9a4df45e7092e17a7025d35808
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-17 20:58:33 +0530

    Replace.js

---
+++ JavaScript/String/Replace.js
@@ -0,0 +1,5 @@
+let str="This is my instagram profile";
+//this replace method will only replaces the first word in the string-->given in parameter the first string is replaced with second string 
+let replacedstr=str.replace("instagram","whatsapp");
+console.log(replacedstr);
+
