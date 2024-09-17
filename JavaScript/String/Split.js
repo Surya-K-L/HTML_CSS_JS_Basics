e6dc141d46c8dc2c535195712dad564ee8701a36
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-17 20:59:26 +0530

    Split.js

---
+++ JavaScript/String/Split.js
@@ -0,0 +1,9 @@
+let str="I learnt javascript in w3Schools";
+//split using space
+let splittedstr=str.split(" ");
+console.log(splittedstr);
+
+//split using comma
+let str1="I learnt javascript,it is very useful in performing differnt actions";
+let splittedstr1=str1.split(",");
+console.log(splittedstr1);
