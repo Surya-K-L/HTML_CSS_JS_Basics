161af0291770ebe771c588946c53210f1aac7da9
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-17 20:58:54 +0530

    ReplaceAll.js

---
+++ JavaScript/String/ReplaceAll.js
@@ -0,0 +1,4 @@
+let str="I am Learning JavaScript in Udemy. Udemy provides detailed description of JavaScript";4
+//replaceAll method replaces all the words in the string --> given in parameter the first string is replaced with second string 
+let replacedstr=str.replaceAll("Udemy","W3schools");
+console.log(replacedstr);
