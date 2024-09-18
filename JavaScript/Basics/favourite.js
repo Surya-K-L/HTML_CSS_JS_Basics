50c7d86db0e80c89e80dae31efc636acbd011f2e
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-19 05:25:07 +0530

    Create favourite.js

---
+++ JavaScript/Basics/favourite.js
@@ -0,0 +1,11 @@
+let factor="Vijay";
+let fplayer="Dhoni";
+let fmovie="Leo";
+
+function favourite()
+{
+    console.log("Favourite Actor:"+factor);
+    console.log("Favourite Player:"+fplayer);
+    console.log("Favourite Movie:"+fmovie);
+}
+favourite();
