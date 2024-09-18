16b9c38dfd0878e49728e8941d6550bf9daba1b4
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-18 15:18:17 +0530

    StringSlice.js

---
+++ JavaScript/String/StringSlice.js
@@ -0,0 +1,7 @@
+let str="JavaScript is used to perform actions";
+let slice=str.slice(3,12);
+console.log(slice);
+
+//if negative value is given the word is taken from last
+let slice1=str.slice(-8);
+console.log(slice1);
