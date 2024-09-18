93cf25019f3883d7d4f0ac65ce7b28939244e1b9
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-19 05:25:42 +0530

    Create func.js

---
+++ JavaScript/Basics/func.js
@@ -0,0 +1,15 @@
+function samsung()
+{
+    console.log("This is samsung");
+}
+function vivo()
+{
+    console.log("This is vivo");
+}
+function iphone()
+{
+    console.log("This is iphone");
+}
+samsung();
+vivo();
+iphone();
