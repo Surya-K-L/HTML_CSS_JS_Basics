85391d373fbd68810feb65ea60d2b96d0999aa6b
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-18 15:12:45 +0530

    IndexOf.js

---
+++ JavaScript/String/IndexOf.js
@@ -0,0 +1,7 @@
+let str="I am going to learn web development";
+//if the index word is found it will return the index position else it will return the -1 value 
+let index=str.indexOf("learn");
+console.log(index);
+
+let index1=str.indexOf("javascript");
+console.log(index1);
