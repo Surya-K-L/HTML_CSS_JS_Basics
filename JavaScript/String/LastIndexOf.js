bc380259dbcded858b69bbea97c1ff91e04aded4
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-18 15:14:48 +0530

    LastIndexOf.js

---
+++ JavaScript/String/LastIndexOf.js
@@ -0,0 +1,4 @@
+let str="I am going to learn web development in W3Schools . The detailed Description of each method is given in W3Schools";
+//if the index word is found it will return the index position else it will return the -1 value 
+let index=str.lastIndexOf("W3Schools");
+console.log(index);
