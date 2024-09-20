512fe1873153de67ede855bcde6699fd058120e4
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-20 05:33:27 +0530

    Trim.js

---
+++ JavaScript/String/Trim.js
@@ -0,0 +1,12 @@
+let str="      CSS is used for styling webpages   ";
+//trims all the white spaces to single space
+let trimmedstr=str.trim();
+console.log(trimmedstr);
+
+//to remove space only in start
+let trimstart=str.trimStart();
+console.log(trimstart);
+
+//to remove space only in end
+let trimend=str.trimEnd();
+console.log(trimend);
