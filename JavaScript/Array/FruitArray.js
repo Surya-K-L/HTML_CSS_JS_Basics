817a0ccbb1a42e927fbefec4e01044e78546a794
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-21 21:30:43 +0530

    Create FruitArray.js

---
+++ JavaScript/Array/FruitArray.js
@@ -0,0 +1,7 @@
+var fruit=["Apple","Banana","Grapes","Orange"]
+console.log(fruit);
+//print first element alone
+console.log(fruit[0]);
+//print any element in the array using index value
+console.log(fruit[3]);
+
