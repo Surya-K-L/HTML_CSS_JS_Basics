fc62130663e145c870e288dfb89d5c1457092386
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-22 19:56:08 +0530

    Sign.js

---
+++ JavaScript/Math/Sign.js
@@ -0,0 +1,6 @@
+let b=Math.sign(-3);//return -1 as it is negative number
+let c=Math.sign(45);//return 1 as it is positive number
+let d=Math.sign(0);//return 0 
+console.log(b);
+console.log(c);
+console.log(d);
